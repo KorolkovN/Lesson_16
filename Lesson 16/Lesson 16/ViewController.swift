1f32d7beef8d3d81e835c913dd8873d0173c8fe9
KorolkovN <117345088+KorolkovN@users.noreply.github.com>
2023-02-22 18:39:32 +0300

    Add files via upload

---
+++ Lesson 16/Lesson 16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson 16
+//
+//  Created by Никита Корольков on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
